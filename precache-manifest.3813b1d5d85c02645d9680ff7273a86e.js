3be9624309e475444b2862a0c03a2c4b5eb20413
marcosferr <ferreiramarcosadrian@gmail.com>
2023-11-01 16:29:47 -0300

    Updates

---
+++ precache-manifest.3813b1d5d85c02645d9680ff7273a86e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31efc9529329e527df820034312399c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6c21bccb239a455b829",
+    "url": "/static/js/2.bbd0f2fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bbd0f2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5875a7bde3dfb24b56d2",
+    "url": "/static/js/main.33ea785d.chunk.js"
+  },
+  {
+    "revision": "af92728125c7e8afc395",
+    "url": "/static/js/runtime-main.204e55fa.js"
+  },
+  {
+    "revision": "90f4b7d148a8279153e2b114ee3ca662",
+    "url": "/static/media/profile.90f4b7d1.png"
+  },
+  {
+    "revision": "9aeeb86fa93a7c611acf7b1a66a28c27",
+    "url": "/static/media/project1.9aeeb86f.png"
+  },
+  {
+    "revision": "5b40bab96e5826bfca5c0812c0ad37a5",
+    "url": "/static/media/project2.5b40bab9.png"
+  },
+  {
+    "revision": "5b40bab96e5826bfca5c0812c0ad37a5",
+    "url": "/static/media/project3.5b40bab9.png"
+  }
+]);
