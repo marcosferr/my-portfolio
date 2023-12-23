f2deb2fcb4e875307b02e8df0780bfe85e6ba117
marcosferr <ferreiramarcosadrian@gmail.com>
2023-12-23 11:01:34 -0300

    Prepare for heroku deployment

---
+++ build/precache-manifest.74c170a01ed0b77176ee0ba5f77ba3fe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652d951ae531780b8ef8464d26d2a273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "770de8931d102e6ae8a4",
+    "url": "/static/js/2.ed3dcb4f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed3dcb4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6034193bdf7ca99f8a5",
+    "url": "/static/js/main.d0dc6ff2.chunk.js"
+  },
+  {
+    "revision": "31fe52e0bb68f83722f1",
+    "url": "/static/js/runtime-main.7094e3bb.js"
+  },
+  {
+    "revision": "daf1360d57942a8fd533b28ae6b00b48",
+    "url": "/static/media/profile.daf1360d.png"
+  },
+  {
+    "revision": "9aeeb86fa93a7c611acf7b1a66a28c27",
+    "url": "/static/media/project1.9aeeb86f.png"
+  },
+  {
+    "revision": "377f20631fa45d3916c0799cd8f1a2ec",
+    "url": "/static/media/project2.377f2063.png"
+  },
+  {
+    "revision": "b95af24ab86b5cf1ec26e5546285af7c",
+    "url": "/static/media/project3.b95af24a.png"
+  },
+  {
+    "revision": "c8f3f35fc82f8ab69e0dda74323d3072",
+    "url": "/static/media/robi.c8f3f35f.png"
+  }
+]);
